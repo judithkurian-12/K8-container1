1acd3b0e33db0f9d3188eb722f486f4b4f58b107
Judith Kurian <judith.kurian@foodhub.com>
2024-07-12 18:22:58 -0300

    check

--- app.py
+++ app.py
@@ -1,52 +1,122 @@
-# container1/app.py
+# # container1/app.py
+# from flask import Flask, request, jsonify
+# import requests
+# import os
+
+# app = Flask(__name__)
+
+# PERSISTENT_STORAGE_PATH = '/judith_PV_dir'
+
+# @app.route('/store-file', methods=['POST'])
+# def store_file():
+#     data = request.get_json()
+
+#     # Validate JSON input
+#     if 'file' not in data or not data['file']:
+#         return jsonify({"file": None, "error": "Invalid JSON input."})
+
+#     file_name = data['file']
+#     file_data = data.get('data', '')
+
+#     file_path = os.path.join(PERSISTENT_STORAGE_PATH, file_name)
+
+#     try:
+#         with open(file_path, 'w') as file:
+#             file.write(file_data)
+#         return jsonify({"file": file_name, "message": "Success."})
+#     except Exception as e:
+#         return jsonify({"file": file_name, "error": "Error while storing the file to the storage."})
+
+# @app.route('/calculate', methods=['POST'])
+# def calculate():
+
+#     data = request.get_json()
+
+#     # Validate JSON input
+#     if 'file' not in data or data['file'] is None:
+#         return jsonify({"file": None, "error": "Invalid JSON input."})
+
+#     file_name = data['file']
+#     product = data.get('product', '')
+
+#     # Check if file exist
+#     file_path = os.path.join('/judith_PV_dir', file_name)
+#     if not os.path.exists(file_path):
+#         return jsonify({"file": file_name, "error": "File not found."})
+
+#     # Send the request to Container 2
+#     response = requests.post('http://localhost:7000/sum', json={"file": file_name, "product": product})
+#     return jsonify(response.json())
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=6000)
+
 from flask import Flask, request, jsonify
 import requests
 import os
 
 app = Flask(__name__)
+port = 6000
 
-PERSISTENT_STORAGE_PATH = '/judith_PV_dir'
+def store_file_in_storage(file_name, data):
+    #parent_dir = os.path.dirname(app.root_path)  # Get the parent directory of the application root
+    #data_dir = os.path.join(parent_dir, 'data')  # Get the path to the 'data' directory
+    try:
+        if not os.path.exists('/judith_PV_dir'):
+            os.makedirs('/judith_PV_dir')
+        file_path = os.path.join('/judith_PV_dir', file_name)
+        #os.makedirs('./data', exist_ok=True)
+        with open(file_path, 'w') as file:
+            file.write(data)
+        return True
+    except Exception as e:
+        print(e.message)  # Print the error for debugging purposes
+        return False
 
 @app.route('/store-file', methods=['POST'])
-def store_file():
-    data = request.get_json()
+def store():
+    if not request.is_json:
+        return jsonify({"file": None, "error": "Invalid JSON input."}), 400
+    request_data = request.json
 
-    # Validate JSON input
-    if 'file' not in data or not data['file']:
-        return jsonify({"file": None, "error": "Invalid JSON input."})
+    if 'file' not in request_data or 'data' not in request_data:
+        return jsonify({"file": None, "error": "Invalid JSON input."}), 400
 
-    file_name = data['file']
-    file_data = data.get('data', '')
+    # Extract file name and data from JSON
+    file_name = request_data['file']
+    data = request_data['data']
 
-    file_path = os.path.join(PERSISTENT_STORAGE_PATH, file_name)
+    if file_name == None or file_name == "":
+        return jsonify({"file": None, "error": "Invalid JSON input."}), 400
 
-    try:
-        with open(file_path, 'w') as file:
-            file.write(file_data)
-        return jsonify({"file": file_name, "message": "Success."})
-    except Exception as e:
-        return jsonify({"file": file_name, "error": "Error while storing the file to the storage."})
+    # Store file in GKE persistent storage
+    if store_file_in_storage(file_name, data):
+        return jsonify({"file": file_name, "message": "Success."}), 200
+    else:
+        return jsonify({"file": file_name, "error": "Error while storing the file to the storage."}), 500
+
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
+    try:
+        inputJSON = request.json
+        file = inputJSON.get('file')
+        print(file)
+        file_path = os.path.join('/judith_PV_dir', file)
+        print('file_path',file_path)
+        print(file == None)
+        if file != None:
+            if not os.path.exists(file_path):
+                return jsonify({"file": file, "error": "File not found."})
+            elif os.path.exists(file_path):
+                print('calling container2')
+                response = requests.post('http://app2-service:7000/calculate', json=inputJSON)
+                return jsonify(response.json())
+        else:
+            return jsonify({"file": file, "error": "Invalid JSON input."})
 
-    data = request.get_json()
-
-    # Validate JSON input
-    if 'file' not in data or data['file'] is None:
-        return jsonify({"file": None, "error": "Invalid JSON input."})
-
-    file_name = data['file']
-    product = data.get('product', '')
-
-    # Check if file exist
-    file_path = os.path.join('/judith_PV_dir', file_name)
-    if not os.path.exists(file_path):
-        return jsonify({"file": file_name, "error": "File not found."})
-
-    # Send the request to Container 2
-    response = requests.post('http://localhost:7000/sum', json={"file": file_name, "product": product})
-    return jsonify(response.json())
+    except Exception as error:
+        return jsonify({"file": file, "error": "Invalid JSON input."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=port, debug=True)
